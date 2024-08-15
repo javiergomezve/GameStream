7f39063fb80c0a7c897fc2c09b80bca0f9633b42
Javier Gomez <javiergomezve@gmail.com>
2024-08-14 21:00:47 -0500

    Initial Commit

---
+++ GameStream/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameStream
+//
+//  Created by Javier Gomez on 14/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
